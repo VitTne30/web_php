17f80ef32948f2c9668b78b9aaaed0494ae74ef9
VitTne30 <97173952+VitTne30@users.noreply.github.com>
2024-07-09 19:31:20 +0700

    Update commend sendmail.php

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -42,7 +42,7 @@ class Mailer{
 			$mail->Body    = $noidung;
 		//    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-			$mail->send();
+			//$mail->send();
 			echo 'Message has been sent';
 		} catch (Exception $e) {
 			echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
